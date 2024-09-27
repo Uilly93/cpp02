b4c004bdeb513ee4f6f01d76af34c7433aabd92e
wnocchi <wnocchi@student.42.fr>
2024-09-27 16:38:01 +0200

    ex02 finished, need to test

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -15,10 +15,15 @@ int main( void ) {
 	std::cout << Fixed::max( a, b ) << std::endl;
 
 	Fixed nb(42.42f);
+	Fixed nb2(2);
 	std::cout << nb << std::endl;
 	std::cout << --nb << std::endl;
 	std::cout << nb << std::endl;
 	std::cout << nb-- << std::endl;
 	std::cout << nb << std::endl;
+
+	std::cout << nb * nb2 << std::endl;
+	std::cout << nb / nb2 << std::endl;
+
 	return 0;
 }
