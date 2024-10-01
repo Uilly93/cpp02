21260abdaefcb36b5a9cc11bd96d82397a767401
wnocchi <wnocchi@student.42.fr>
2024-10-01 09:47:17 +0200

    ex02 finished

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,6 +2,7 @@
 #include "../includes/Fixed.hpp"
 
 int main( void ) {
+	//main test
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
@@ -10,10 +11,12 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << --a << std::endl;
 
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
-
+//
+//my tests
 	Fixed nb(42.42f);
 	Fixed nb2(2);
 	std::cout << nb << std::endl;
@@ -24,6 +27,10 @@ int main( void ) {
 
 	std::cout << nb * nb2 << std::endl;
 	std::cout << nb / nb2 << std::endl;
+	std::cout << nb + nb2 << std::endl;
+	std::cout << nb - nb2 << std::endl;
+	nb = nb2;
+	std::cout << nb << std::endl;
 
 	return 0;
 }
