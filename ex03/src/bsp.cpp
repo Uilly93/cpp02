abddb9df1b792938ec48bed29e0256757576a789
wnocchi <wnocchi@student.42.fr>
2024-10-04 09:10:22 +0200

    module finished

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -1,9 +1,9 @@
 #include "../includes/Point.hpp"
+#include <algorithm>
 
 
 const Fixed	determinant(Point const &a, Point const &b, Point const &d){
 	const Fixed det((a.getx() - d.getx()) * (b.gety() - d.gety()) - (a.gety() -d.gety()) * (b.getx() - d.getx()));
-	std::cout << det << RESET << std::endl;
 	return det;
 }
 
@@ -11,7 +11,10 @@ bool bsp( Point const &a, Point const &b, Point const &c, Point const &point){
 	const Fixed det_ab = determinant(a, b, point);
 	const Fixed det_bc = determinant(b, c, point);
 	const Fixed det_ca = determinant(c, a, point);
-	if (det_ab > 0 && det_bc > 0 && det_ca > 0)
+	if (det_ab > 0 && det_bc > 0 && det_ca > 0){
+		std::cout << GREEN << "is inside the triangle" << RESET << std::endl;
 		return true;
+	}
+	std::cout << RED << "is outside the triangle" << RESET << std::endl;
 	return false;
 }
--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -3,11 +3,25 @@
 
 int main( void ) {
 	//main test
-	const Point a(4, 4);
-	const Point b(0, 0);
-	const Point c(4, 0);
-	const Point d(3, 2);
-	return bsp(a, b, c, d);
+
+	//Triangle point:
+	const Point a(-2.3f, 4);
+	const Point b(-4.68f, 1.37f);
+	const Point c(1.86f, 2.19f);
+
+	//Points testing:
+	const Point d(1.24f, 4.25f);
+	const Point e(-1.6f, 0.37);
+	const Point f(-2.04, 2.69f);
+	const Point g(-4.76f, 4.01f);
+	std::cout << NBLUE << "d " << RESET;
+	bsp(a, b, c, d);
+	std::cout << NBLUE << "e " << RESET;
+	bsp(a, b, c, e);
+	std::cout << NBLUE << "f " << RESET;
+	bsp(a, b, c, f);
+	std::cout << NBLUE << "g " << RESET;
+	bsp(a, b, c, g);
 
 	return 0;
 }
